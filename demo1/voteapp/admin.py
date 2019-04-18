6c5aa3380776c5b4041cf5a34ee37064360197ab
lh <1466947051@qq.com>
2019-04-18 15:26:49 +0800

    后台界面管理完成

--- demo1/voteapp/admin.py
+++ demo1/voteapp/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Problem, Option
 
 # Register your models here.
+
+
+class OptionInline(admin.StackedInline):
+    model = Option
+    extra = 2
+
+
+class OptionAdmin(admin.ModelAdmin):
+    list_display = ['name', 'frequency', 'pro']
+    list_filter = ['oname']
+    search_fields = ['oname']
+    list_per_page = 5
+
+
+class ProblemAdmin(admin.ModelAdmin):
+    # 显示字段
+    list_display = ['name', 'frequency']
+    # 过滤字段
+    list_filter = ['pname', 'pfrequency']
+    # 搜索字段
+    search_fields = ['pname']
+    # 分页
+    list_per_page = 5
+    # 级联创建
+    inlines = [OptionInline]
+
+
+# 注册模型到管理界面
+admin.site.register(Problem, ProblemAdmin)
+admin.site.register(Option, OptionAdmin)
--- demo1/voteapp/models.py
+++ demo1/voteapp/models.py
@@ -7,8 +7,34 @@ class Problem(models.Model):
     pname = models.CharField(max_length=50)
     pfrequency = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.pname
+
+    def name(self):
+        return self.pname
+    name.short_description = '问题'
+
+    def frequency(self):
+        return self.pfrequency
+    frequency.short_description = '参与次数'
+
 
 class Option(models.Model):
     oname = models.CharField(max_length=20)
     ofrequency = models.IntegerField(default=0)
     opro = models.ForeignKey('Problem', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.oname
+
+    def name(self):
+        return self.oname
+    name.short_description = '选项'
+
+    def frequency(self):
+        return self.ofrequency
+    frequency.short_description = '票数'
+
+    def pro(self):
+        return self.opro
+    pro.short_description = '问题'
